da537aedeec1c545b03adf3a74464fa59c65f431
akshaynayakkolgar97@gmail.com <akshaynayakkolgar97@gmail.com>
2025-10-12 14:59:24 -0400

    update design

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,25 @@ export default function RootLayout({ children, emotionCache = clientSideEmotionC
         <link rel="apple-touch-icon" href="/Icons/aknlogo.png" />
         <meta name="theme-color" content="#1B5E20" />
         <title>Akshay K Nayak</title>
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Akshay K Nayak" />
+        <meta property="og:description" content="PhD student at Old Dominion University researching Human-Computer Interaction and Accessibility" />
+        <meta property="og:image" content="/akn.jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Akshay K Nayak" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Akshay K Nayak" />
+        <meta name="twitter:description" content="PhD student at Old Dominion University researching Human-Computer Interaction and Accessibility" />
+        <meta name="twitter:image" content="/akn.jpeg" />
+        <meta name="twitter:image:alt" content="Akshay K Nayak" />
+
+        {/* General Meta Tags */}
+        <meta name="description" content="PhD student at Old Dominion University researching Human-Computer Interaction and Accessibility" />
       </head>
       <body>
         <CacheProvider value={emotionCache}>
